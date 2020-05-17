b11883fc4870c822094650f68a8ac8f7e34b53e5
davidh <david.hertenstein@gmail.com>
2020-05-17 03:37:31 -0400

    update

--- addons/gdutil/source/gdisplay/gdisplay.cpp
+++ addons/gdutil/source/gdisplay/gdisplay.cpp
@@ -9,6 +9,7 @@ GDisplay::~GDisplay() {}
 void GDisplay::_register_methods() {}
 
 void GDisplay::_init() {
+	Godot::print("Hello from GDisplay!");
 #ifndef _DEBUG
 	queue_free();
 #endif
---
+++ source/src/gdexample.hpp
@@ -0,0 +1,26 @@
+#ifndef GDEXAMPLE_H
+#define GDEXAMPLE_H
+
+#include <gdregistry/gdregistry.hpp>
+
+#include <gdutil.hpp>
+#include <scenemanager/scenemanager.hpp>
+
+#include <Godot.hpp>
+#include <Sprite.hpp>
+
+namespace godot {
+
+class GDExample : public Sprite {
+	GDCLASS(GDExample, Sprite)
+
+private:
+	float time_passed;
+
+public:
+	void _process(float delta);
+};
+
+} // namespace godot
+
+#endif
